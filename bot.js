1e877db72ba045e5be3c549c2d0b6a145f8f15ff
maestro4499 <50502784+maestro4499@users.noreply.github.com>
2019-05-11 22:52:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,47 @@ client.on('message', msg => {
   }
 });
 
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);//لا تحط التوكن حقك هنا
+
+
+client.on('ready',  () => {
+
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+
+  console.log('by ADG-RP');
+
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+
+  console.log(`Logged in as  * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
+
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+
+  console.log('is online')
+
+client.user.setStatus("dnd");
+
+});
+
+
+
+
+client.on('ready', function(){
+    var ms = 10000 ;
+    var setGame = ['Broudcast ',' |ADG-RP ' ];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j
+        client.user.setGame(setGame[i],`https://www.twitch.tv/حب بلا حدود`);
+    }, ms);
+
+})
 
 client.on("message", message => {
     if (message.content.startsWith(prefix + "obc")) { ///@» Maestro
